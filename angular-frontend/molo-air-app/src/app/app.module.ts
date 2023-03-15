d8ae48494e05092f91b64da061339650ca097ffd
William Bence <william.bence@entelect.co.za>
2023-03-15 11:38:23 +0200

    Added navbar and routing to different screens

--- angular-frontend/molo-air-app/src/app/app.module.ts
+++ angular-frontend/molo-air-app/src/app/app.module.ts
@@ -3,28 +3,49 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeScreenComponent } from './dashboard/home-screen/home-screen.component';
-import { HomeNavComponent } from './dashboard/home-nav/home-nav.component';
 import { FlightScreenComponent } from './flights/flight-screen/flight-screen.component';
 import { BookingScreenComponent } from './bookings/booking-screen/booking-screen.component';
 import { CustomerScreenComponent } from './customer/customer-screen/customer-screen.component';
 import { CustomerCreateComponent } from './customer/customer-create/customer-create.component';
 import { BookingCreateComponent } from './bookings/booking-create/booking-create.component';
 import { FlightCreateComponent } from './flights/flight-create/flight-create.component';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './general/navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeScreenComponent,
-    HomeNavComponent,
     FlightScreenComponent,
     BookingScreenComponent,
     CustomerScreenComponent,
     CustomerCreateComponent,
     BookingCreateComponent,
-    FlightCreateComponent
+    FlightCreateComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot([
+      {path: '', component : HomeScreenComponent},
+      {path: 'bookings', component: BookingScreenComponent},
+      {path: 'flights', component: FlightScreenComponent},
+      {path: 'customers', component: CustomerScreenComponent}
+    ]),
+    BrowserModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-frontend/molo-air-app/src/app/general/navbar/navbar.component.spec.ts
+++ angular-frontend/molo-air-app/src/app/general/navbar/navbar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeNavComponent } from './home-nav.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('HomeNavComponent', () => {
-  let component: HomeNavComponent;
-  let fixture: ComponentFixture<HomeNavComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeNavComponent ]
+      declarations: [ NavbarComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HomeNavComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

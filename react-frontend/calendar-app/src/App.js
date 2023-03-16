1d7151b7d63b89af3b82feb3eb1cfcaf79cdb085
William Bence <william.bence@entelect.co.za>
2023-03-16 09:42:47 +0200

    Added button and typography components

--- react-frontend/calendar-app/src/App.js
+++ react-frontend/calendar-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { MuiTypography } from './components/MuiTypography';
+import { MuiButton } from './components/MuiButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
+        {/* <MuiTypography></MuiTypography> */}
+        <MuiButton/>
       </header>
     </div>
   );
---
+++ react-frontend/calendar-app/src/components/MuiButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Button} from '@mui/material'
+export const MuiButton = () => {
+  return (
+    <div>
+        <Button variant='text'>Text</Button>
+        <Button variant='contained'>contained</Button>
+        <Button variant='outlined'>outlined</Button>
+    </div>
+  )
+}

8624764f7b1c8407946db0d196edbe11fde4578c
William Bence <william.bence@entelect.co.za>
2023-03-20 08:19:39 +0200

    Frontend changes to customer screen

--- angular-frontend/molo-air-app/src/app/customer/customer-screen/customer-screen.component.ts
+++ angular-frontend/molo-air-app/src/app/customer/customer-screen/customer-screen.component.ts
@@ -3,6 +3,7 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { CustomerEndpointsService } from 'src/app/endpoints/customer/customer-endpoints.service';
 import { Customer } from 'src/app/models/customer';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-customer-screen',
@@ -55,4 +56,57 @@ export class CustomerScreenComponent implements OnInit{
 
   @Input()
   displayedColumns: string[] = ['customerId', 'username', 'firstName', 'lastName', 'passportNumber', 'email','phoneNumber'];
+
+  
+  async onAddCustomerClick(): Promise<void> {
+    const steps = ['1', '2', '3', '4' ,'5'];
+    const stepNames = ['Username','Full Name', 'Passport Number','Email Address','Phone Number'];
+    const swalQueueStep = Swal.mixin({
+      confirmButtonText: 'Ok',
+      cancelButtonText: 'Back',
+      progressSteps: steps,
+      input: 'text',
+      inputAttributes: {
+        required: 'true'
+      },
+      reverseButtons: true,
+      validationMessage: 'This field is required'
+    })
+    
+    const values = []
+    let currentStep
+    
+    for (currentStep = 0; currentStep < steps.length;) {
+      const result : any = await swalQueueStep.fire({
+        title: `Please enter ${stepNames[currentStep]}`,
+        inputValue: values[currentStep],
+        showCancelButton: currentStep > 0,
+        currentProgressStep: currentStep
+      })
+    
+      if (result.value) {
+        values[currentStep] = result.value
+        currentStep++
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        currentStep--
+      } else {
+        break
+      }
+    }
+    
+    if (currentStep === steps.length) {
+      this.customers.push(
+      { id: 1,
+        username: values[0],
+        firstName: values[1].split(' ')[0],
+        lastName: values[1].split(' ')[1],
+        passportNumber: values[2],
+        email: values[3],
+        phoneNumber: values[4]
+      });
+      console.log(this.customers);
+      this.dataSource.data = this.customers;
+    }
 }
+}
+
